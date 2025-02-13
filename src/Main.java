052df9375835974736c78e50eb2e3b93035b307b
jmalarconn <jmalarcon@ucundinamarca.edu.co>
2025-02-13 17:18:40 -0500

    Primer ejemplo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
         }
     }
